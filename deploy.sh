2f4bc6dae32ab3ac03906db1b58cea5e1a6f7c73
Anders Sundman <anders@4zm.org>
2014-01-11 19:50:04 +0100

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to deploy the latest version from github
+
+echo "[+] Geting files from github"
+
+git pull
+
+echo "[+] Removing old files"
+
+rm -rf www
+mkdir www
+
+echo "[+] Copying new files"
+
+cp cc-by-nc.png www/
+cp index.html www/
+cp index.en.html www/
+cp cryptoparty_logo.png www/
+cp cryptoparty.css www/
+
+echo "[+] It is done"
